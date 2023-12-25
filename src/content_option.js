e491060eb8c24557f7a968b801c46de0c3c665c8
yeakiniqra <108855634+yeakiniqra@users.noreply.github.com>
2023-12-25 20:18:28 +0600

    added

---
+++ src/content_option.js
@@ -0,0 +1,130 @@
+const logotext = "IQRA";
+const meta = {
+    title: "Yeakin Iqra",
+    description: "I’m a FrontEnd Developer based in Dhaka, Bangladesh.",
+};
+
+const introdata = {
+    title: "I’m Yeakin Iqra",
+    animated: {
+        first: "I love coding",
+        second: "I develop cool websites",
+        third: "I develop react apps",
+    },
+    description: "I create elevating digital experience that inspire & connect with people through Development & Design",
+    your_img_url: "https://i.postimg.cc/bw6ScP4T/about-img.webp",
+};
+
+const dataabout = {
+    title: "about my self",
+    aboutme: "I am an Entry-Level FrontEnd Developer. I can make simple websites for you using HTML,CSS & Javascript.I have also experience in frameworks like tailwind css & React JS.I also worked with Java & MySql.",
+};
+const worktimeline = [{
+        jobtitle: "Intern",
+        where: "Eutropia IT Solution",
+        date: "2023",
+    },
+    {
+        jobtitle: "Video Editor",
+        where: "ARTBOOK",
+        date: "2021-present",
+    }
+];
+
+const skills = [{
+        name: "Python",
+        value: 60,
+    },
+    {
+        name: "Django",
+        value: 65,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Tailwind css",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "WEB DESIGN",
+        description: "I can design a website for you using HTML,CSS & Javascript",
+    },
+    {
+        title: "Web Apps",
+        description: "I can develop a web app for you using React JS & Tailwind css",
+    },
+    {
+        title: "Video Editor",
+        description: "I can edit your video using Adobe Premiere Pro & After Effects",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.postimg.cc/3R3mpLjW/project-1.webp",
+        description: "This is based on React js & Tailwind css.Used TMDb API for movie data",
+        link: "https://moviefiesta.netlify.app/",
+    },
+    {
+        img: "https://i.postimg.cc/brJnVsBh/project-2.webp",
+        description: "This is based on React js & Tailwind css.Used static data for car rent",
+        link: "https://rentcarswift.netlify.app/",
+    },
+    {
+        img: "https://i.postimg.cc/JzJkFwPG/project-3.webp",
+        description: "This is based on React js,Tailwind css & Daisy UI.Used static data for details",
+        link: "https://pccsite.vercel.app/",
+    },
+    {
+        img: "https://i.postimg.cc/tCsPhn6d/project-4.webp",
+        description: "This is based on React js & Tailwind css.Used static data for details also used react toastify",
+        link: "https://easystayhub.vercel.app/",
+    },
+    {
+        img: "https://i.postimg.cc/NF5y7ZKG/projects5.webp",
+        description: "This is based on HTML,CSS & JS for design & functionality",
+        link: "https://yeakiniqra.github.io/Pharmacy/",
+    },
+    {
+        img: "https://i.postimg.cc/D0HSC5Gr/projects6.webp",
+        description: "This is based on HTML,CSS & JS for design & functionality",
+        link: "https://yeakiniqra.github.io/DreamScapes/",
+    }
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "yeakiniqra.connect@gmail.com",
+    YOUR_FONE: "01785939572",
+    description: "Would you like to work with me? Awesome!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_h78bucd",
+    YOUR_TEMPLATE_ID: "template_74gjdqb",
+    YOUR_USER_ID: "bTERfKaqYKJv3lza8",
+};
+
+const socialprofils = {
+    github: "https://github.com/yeakiniqra",
+    facebook: "https://www.facebook.com/yeakin.iqra/",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com/YeakinIqra",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
